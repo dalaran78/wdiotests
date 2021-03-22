fe02de8720eb794df5ae4c4a16e543c54b6f8190
Sergei Svistelnikov <sergei.svistelnikov@outlook.com>
2021-03-22 16:39:50 -0700

    Test cases 1st homework

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,10 +1,18 @@
 import Page from './page';
 
-
 class LoginPage extends Page {
     get inputUsername () { return $('#normal_login_email'); }
+    get clearUsername () { return $('#normal_login_email'); }
     get inputPassword () { return $('#normal_login_password'); }
+    get clearPassword () { return $('#normal_login_password'); }
     get btnSubmit () { return $('.login-form-button'); }
+    get errorToast () { return $('.ant-notification-notice-message'); }
+    get alertMessage () { return $('.ant-form-item-explain-error')}
+
+
+    open () {
+        return super.open('/');
+    }
 
     setLogin  (email) {
         this.inputUsername.setValue(email);
@@ -18,8 +26,28 @@ class LoginPage extends Page {
         this.btnSubmit.click();
     }
 
-    open () {
-        return super.open('/');
+    isDisabled() {
+        expect(this.btnSubmit).toBeDisabled();
+    }
+
+    errorToastAppeared() {
+        expect(this.errorToast).toBeDisplayed();
+    }
+
+    loginClear() {
+        this.clearUsername.clearValue();
+    }
+
+    passClear() {
+        this.clearPassword.clearValue();
+    }
+
+    alert() {
+        expect(this.alertMessage === 'Required');
+    }
+
+    alertEmail() {
+        expect(this.alertMessage === '\'email\' is not a valid email');
     }
 }
 
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -2,14 +2,52 @@ import LoginPage from  '../pageobjects/login.page';
 import ProfilePage from  '../pageobjects/profile.page';
 
 describe('Auth', () => {
-    it('User login with valid data', () => {
+    beforeEach( () => {
         LoginPage.open();
+        browser.execute('window.localStorage.clear()');
+        browser.refresh();
+    });
+
+    it('TC-001 | User login with valid data', () => {
         LoginPage.setLogin('fewodes573@990ys.com');
         LoginPage.setPassword('qwerty');
         LoginPage.clickSubmitButton();
         ProfilePage.isOpen();
     });
+
+    it('TC-002 | Submit button should be disabled when input fields are empty', () => {
+        LoginPage.isDisabled();
+    });
+
+    it('TC-003 | Auth should be failed after invalid data provided', () => {
+        LoginPage.setLogin('test@test.com');
+        LoginPage.setPassword('qwerty');
+        LoginPage.clickSubmitButton();
+        LoginPage.errorToastAppeared();
+    });
+
+    it('TC-004 | "Required" alert should appear under Login field when input data is cleared', () => {
+        LoginPage.setLogin('test@test.com');
+        LoginPage.loginClear();
+        LoginPage.alert();
+    });
+
+    it('TC-005 | "Required" alert should appear under Password field when input data is cleared', () => {
+        LoginPage.setPassword('qwerty');
+        LoginPage.passClear();
+        LoginPage.alert();
+    });
+
+    it('TC-006 | "\'email\' is not a valid email" alert should appear under Login field when wrong value entered', () => {
+        LoginPage.setLogin('fewodes573990ys.com');
+        LoginPage.alertEmail();
+    });
+
+    afterEach( () => {
+        browser.execute('window.localStorage.clear()');
+    });
 });
+
 
 
 
